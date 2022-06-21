1081509d8bf6cd1e79ebda94f43fc7dd12bd4de4
AnharAwadh <anhar9617@gmail.com>
2022-06-21 20:31:24 +0300

    add the hw react day 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
+import Resulit from "./componet/Resulit";
 function App() {
+  const [input1, setInput1] = useState();
+  const [input2, setInput2] = useState();
+  const [resulit, setResulit] = useState();
+
+  const doCalculte = (operter) => {
+    if (operter === "+") {
+      setResulit(parseInt(input1) + parseInt(input2));}
+    if (operter === "-") {
+      setResulit(parseInt(input1) - parseInt(input2));}
+   if (operter === "/") {
+        setResulit(parseInt(input1) / parseInt(input2));}
+    if (operter === "*") {
+          setResulit(parseInt(input1) * parseInt(input2));}
+   if (operter === "%") {
+            setResulit(parseInt(input1) % parseInt(input2));}
+    
+    }
+    
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="contniers">
+        <h2>calculter </h2>
+        <input
+          type="text"
+          onChange={(e) => setInput1(e.target.value)}
+          className="form-control"
+          aria-label="Server"
+        />
+        <input
+          type="text"
+          onChange={(e) => setInput2(e.target.value)}
+          className="form-control mt-2"
+          aria-label="Server"
+        />
+        <button
+          type="button"
+          onClick={() => doCalculte("+")}
+          className="btn btn-secondary me-md-2 mt-3 g-col-4 "
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          +
+        </button>
+        <button
+          type="button"
+          onClick={() => doCalculte("-")}
+          className="btn btn-secondary me-md-2 mt-3 g-col-4"
+        >
+          -
+        </button>
+        <button type="button"
+         onClick={() => doCalculte("/")}
+         className="btn btn-secondary me-md-2 mt-3">
+          /
+        </button>
+        <button type="button" 
+         onClick={() => doCalculte("*")}
+         className="btn btn-secondary me-md-2 mt-3">
+          *
+        </button>
+        <button type="button" 
+         onClick={() => doCalculte("%")}
+         className="btn btn-secondary me-md-2 mt-3">
+          %
+        </button>
+       <Resulit Resulit={resulit}/>
+      </div>
+    </>
   );
 }
 
